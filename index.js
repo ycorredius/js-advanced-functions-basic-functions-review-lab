ceb01eefb467974f73bf47b54fa9c82b9d318998
Shaun Yarbrough <ycorredius@gmail.com>
2020-03-18 18:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
-function mondayWork(activity= "go to the office"){
+let mondayWork = function (activity= "go to the office"){
   return `This Monday, I will ${activity}.`
 }
